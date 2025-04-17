a9155541d859c54a12a1c9cc58bb7f1275d23164
tigerplush <christopher.putz@corgibase.de>
2025-04-17 09:04:04 +0200

    First "working" version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bevy_2d_template
+
+This is a crates-based implementation of the bevy_flocks bevy_2d_template.
+
+Create new crates with `cargo new --lib  ./crates/new_crate --vcs none`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+// Disable console on Windows for non-dev builds.
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+
+use bevy::prelude::*;
+use bevy_2d_template::AppPlugin;
+
+fn main() -> AppExit {
+    App::new().add_plugins(AppPlugin).run()
 }
